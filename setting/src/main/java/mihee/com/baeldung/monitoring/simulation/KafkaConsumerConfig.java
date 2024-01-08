e0221d5374fd68def8a74df0ca5468d7ade0089f
mihee <algml9603@gmail.com>
2024-01-08 23:02:39 +0900

    feature: 카프카 monitoring

--- setting/src/main/java/mihee/com/baeldung/monitoring/simulation/KafkaConsumerConfig.java
+++ setting/src/main/java/mihee/com/baeldung/monitoring/simulation/KafkaConsumerConfig.java
@@ -17,8 +17,8 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
 @EnableKafka
+@Component
 public class KafkaConsumerConfig {
     @Value(value="${monitory.kafka.bootstrap.config}")
     private String bootstrapAddress;
